500b018d7f6e541e8b54ec2975260bdad807be86
Guilherme <guilhermehenrike0412@gmail.com>
2026-07-24 12:35:39 -0300

    feat: adicionando spring security e criptografia de senhas

--- demo/src/main/java/com/euPassei/demo/Service/UsersService.java
+++ demo/src/main/java/com/euPassei/demo/Service/UsersService.java
@@ -4,10 +4,14 @@ import com.euPassei.demo.DTO.Users.UsersDTO;
 import com.euPassei.demo.Entity.Users;
 import com.euPassei.demo.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UsersRepository usersRepository;
@@ -19,6 +23,8 @@ public class UsersService {
            throw new IllegalArgumentException("O usuario já existe");
         }
 
+        String senhaCriptografada = passwordEncoder.encode(user.getPassword());
+        user.setPassword(senhaCriptografada);
         return usersRepository.save(user);
     }
 
@@ -29,8 +35,10 @@ public class UsersService {
             throw new IllegalArgumentException("Nome ou senha incorretos");
         }
 
-        if  (!usuarioAchado.getPassword().equals(user.getPassword())) {
-            throw new IllegalArgumentException("Nome ou senha incorretos");
+        // se a senha que o usuario passou quando criptografada não for igual a senha do usuario
+        // que está no banco (já criptografada), ele entra nesse erro
+        if (!passwordEncoder.matches(user.getPassword(), usuarioAchado.getPassword())) {
+            throw new IllegalArgumentException("nome ou senha incorretos");
         }
 
         return usuarioAchado;
